c2b3f152ce808086ba26ec803f84d42ee2cf95e4
mbbsadmissionsinabroad <73527273+mbbsadmissionsinabroad@users.noreply.github.com>
2023-07-06 14:02:03 +0530

    Update _document.js
    Google Ads Code

--- pages/_document.js
+++ pages/_document.js
@@ -8,6 +8,15 @@ export default class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
+	    <!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=AW-11026638091"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'AW-11026638091');
+</script>
           <link
             rel="stylesheet"
             href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
