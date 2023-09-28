534e88d1dbf730ece9e05900e6d48eacb9f74200
Abhinav Anand <abhinav.anand500@gmail.com>
2023-09-28 23:05:57 +0530

    Working 2

--- components/HomePage/Testimonials/index.js
+++ components/HomePage/Testimonials/index.js
@@ -1,38 +1,68 @@
-import styles from "../homePage.module.css";
-import {Box, Grid, Typography, CircularProgress} from "@mui/material";
-import CarouselController from "../../CarouselController";
-import { urlFor } from "../../../lib/client";
+import styles from '../homePage.module.css'
+import { Box, Grid, Typography, CircularProgress } from '@mui/material'
+import CarouselController from '../../CarouselController'
+import { urlFor } from '../../../lib/client'
 
-function index(props) {
-	const {testimonials} = props;
-	const data = typeof testimonials === 'object' && testimonials.review;
-	const responsive = {
-		0: 1,
-		960: 2,
-	}
+function index (props) {
+  const { testimonials } = props
+  const data = typeof testimonials === 'object' && testimonials.review
+  const responsive = {
+    0: 1,
+    960: 2
+  }
 
-	if(typeof data !== "object") return <CircularProgress />
-	return (
-		<Box sx={{margin: "30px auto 0px auto"}}>
-			<Box sx={{textAlign: "center"}}>
-				<Typography variant="h2" className={styles.title} id="testimonials" sx={{color: "primary.main"}}>
-					Testimonials
-				</Typography>
-			</Box>
-			<CarouselController show={responsive} infiniteLoop>
-				{
-					data.map((item, i) => (
-						<Box sx={{ display: 'flex', flexDirection: 'column', alignItems:'center', textAlign: 'center', padding:'10px' }} key={i}>
-							<img src={urlFor(item.studentImage)} className={styles.studentImage} />
-							<Typography variant="body1" sx={{ fontSize:'18px' }}>{item.studentName}</Typography>
-							<Typography variant="body1" gutterBottom sx={{ color: 'primary.blue', fontSize:'14px' }}>{item.collegeName}, {item.country}</Typography>
-							<Typography variant="body1" gutterBottom sx={{ fontSize:'15px', padding:'10px' }}>{item.studentTestimonial}</Typography>
-						</Box>
-					))
-				}
-			</CarouselController>
-		</Box>
-	);
+  if (typeof data !== 'object') return <CircularProgress />
+  return (
+    <Box sx={{ margin: '30px auto 0px auto' }}>
+      <Box sx={{ textAlign: 'center' }}>
+        <Typography
+          variant='h2'
+          className={styles.title}
+          id='testimonials'
+          sx={{ color: 'primary.main' }}
+        >
+          Testimonials
+        </Typography>
+      </Box>
+      <CarouselController show={responsive} infiniteLoop>
+        {data.map((item, i) => (
+          <Box
+            sx={{
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+              textAlign: 'center',
+              padding: '10px'
+            }}
+            key={i}
+          >
+            <img
+              src={urlFor(item.studentImage)}
+              className={styles.studentImage}
+              alt={item.metaTitle}
+            />
+            <Typography variant='body1' sx={{ fontSize: '18px' }}>
+              {item.studentName}
+            </Typography>
+            <Typography
+              variant='body1'
+              gutterBottom
+              sx={{ color: 'primary.blue', fontSize: '14px' }}
+            >
+              {item.collegeName}, {item.country}
+            </Typography>
+            <Typography
+              variant='body1'
+              gutterBottom
+              sx={{ fontSize: '15px', padding: '10px' }}
+            >
+              {item.studentTestimonial}
+            </Typography>
+          </Box>
+        ))}
+      </CarouselController>
+    </Box>
+  )
 }
 
-export default index;
+export default index
--- components/Layout.js
+++ components/Layout.js
@@ -1,20 +1,33 @@
-import Navbar from "./Navbar";
-import Footer from "./Footer";
-import { Whatsapp } from "mdi-material-ui";
+import Navbar from './Navbar'
+import Footer from './Footer'
+import { Whatsapp } from 'mdi-material-ui'
 
-export default function Layout({children, data}) {
-	let footerData = data !== undefined ? data.result.find((item) => item.title === 'Footer') : []
-	let headerData = data !== undefined ? data.result.filter((item) => item.title !== 'Footer' && item.title !== 'Home Page Slider' && item.title !== 'Testimonials' && item.title !== 'Contents') : []
-	return (
-		<>
-			<Navbar headerData={headerData} />
-			<main className="mainDiv">{children}</main>
-			<Footer footerData={footerData} />
-			<div className="whatsappBtn" style={{ position: "fixed", zIndex: 9, bottom: '3em', left: '20px' }}>
-				<a href="https://wa.me/+918050006077" target="_blank">
-					<img src="/assests/whatsapp.png" />
-				</a>
-			</div>
-		</>
-	);
+export default function Layout ({ children, data }) {
+  let footerData =
+    data !== undefined ? data.result.find(item => item.title === 'Footer') : []
+  let headerData =
+    data !== undefined
+      ? data.result.filter(
+          item =>
+            item.title !== 'Footer' &&
+            item.title !== 'Home Page Slider' &&
+            item.title !== 'Testimonials' &&
+            item.title !== 'Contents'
+        )
+      : []
+  return (
+    <>
+      <Navbar headerData={headerData} />
+      <main className='mainDiv'>{children}</main>
+      <Footer footerData={footerData} />
+      <div
+        className='whatsappBtn'
+        style={{ position: 'fixed', zIndex: 9, bottom: '3em', left: '20px' }}
+      >
+        <a href='https://wa.me/+918050006077' target='_blank'>
+          <img src='/assests/whatsapp.png' alt='whatsappImg' />
+        </a>
+      </div>
+    </>
+  )
 }
