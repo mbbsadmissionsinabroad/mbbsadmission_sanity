b1e3b394ed9536e719a8c5605d6313b359091ecf
Abhinav Anand <abhinav@Abhinavs-MacBook-Air.local>
2023-10-31 01:30:07 +0530

    Schema added

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,35 @@ import HomePage from '../components/HomePage'
 import Head from 'next/head'
 
 function index (props) {
+  let schema = {
+    "@context": "https://schema.org",
+    "@type": "Article",
+    "mainEntityOfPage": {
+      "@type": "WebPage",
+      "@id": "example.com/articleURL"
+    },
+    "headline": "Article title",
+    // "image": [
+    //   spContent.metaData.twitterCard
+    //  ],
+    "datePublished": "21-10-2001",
+    "dateModified": "21-02-2023",
+    "author": [{
+        "@type": "Person",
+        "name": "Json Bourne",
+        "url": "example.com/jbourne"
+      }],
+    "publisher": {
+        "@type": "Organization",
+        "name": "abc",
+        "logo": {
+          "@type": "ImageObject",
+          "url": "example.com/image"
+        }
+      },
+    "description": "some description"
+  
+  };
   const { sliderData, testimonials, homePageContents } = props
   return (
     <div>
@@ -17,6 +46,9 @@ function index (props) {
         <meta name='keywords' content='MBBS Admission in Abroad' />
         <link rel='canonical' href='https://www.mbbsadmissionsinabroad.com/' />
         <meta name='viewport' content='width=device-width, initial-scale=1.0' />
+        <script 
+            type='application/ld+json'
+            dangerouslySetInnerHTML={{ __html: JSON.stringify(schema) }} />
       </Head>
       <Slider sliderData={sliderData} />
       <HomePage
