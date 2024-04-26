632299587810806b43ea13a028e6e01c17b040c5
Abhinav Anand <abhinav@Abhinavs-MacBook-Air.local>
2024-04-26 12:13:29 +0530

    Color

--- components/TopBar.js
+++ components/TopBar.js
@@ -4,9 +4,10 @@ import { Phone, EmailOutline } from "mdi-material-ui";
 function TopBar() {
   return (
     <div className="topBarBg flex items-center">
-      <div className="flex-grow"></div>
-      <p className="flex h-10 items-center justify-center px-4 text-md font-bold text-yellow-300 sm:px-6 lg:px-8">
-        The Vaidya Vigyan Scholarships worth 1 Crore! (*T&C apply). Explore Now!
+      {/* <div className="flex-grow"></div> */}
+      <p className="flex h-10 items-center flex-grow justify-center px-4 text-lg font-bold text-yellow-300 sm:ml-20 lg:px-8 textBounce">
+        * The Vaidya Vigyan Scholarships worth 1 Crore!{" "}
+        <span className="text-xs">(*T&C apply)</span>. Explore Now! *
       </p>
       <p className="flex items-center ml-4">
         <Phone sx={{ mr: 1 }} />
