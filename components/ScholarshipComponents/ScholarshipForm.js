1bbb040f7792ae50c5d8c935936d54b3340864c7
Abhinav Anand <abhinav@Abhinavs-MacBook-Air.local>
2024-05-08 23:19:36 +0530

    Double Banner

--- components/ScholarshipComponents/ScholarshipForm.js
+++ components/ScholarshipComponents/ScholarshipForm.js
@@ -146,7 +146,7 @@ export default function Example() {
         body: JSON.stringify(data),
       };
 
-      fetch("https://admission-backend.vercel.app/send-email", requestOptions)
+      fetch("https://admission-backend.vercel.app/send-emaill", requestOptions)
         .then((res) => res.json())
         .then((data) => {
           setFormData({
@@ -312,6 +312,7 @@ export default function Example() {
                       type="number"
                       required={true}
                       name="studentMobileNumber"
+                      minlength="5"
                       id="studentMobileNumber"
                       value={formData.studentMobileNumber}
                       onChange={handleChange}
@@ -352,6 +353,7 @@ export default function Example() {
                   <div className="mt-2">
                     <input
                       type="number"
+                      minlength="5"
                       required={true}
                       name="fatherMobileNumber"
                       id="fatherMobileNumber"
@@ -372,6 +374,7 @@ export default function Example() {
                   <div className="mt-2">
                     <input
                       type="number"
+                      minlength="5"
                       required={true}
                       name="motherMobileNumber"
                       id="motherMobileNumber"
@@ -602,7 +605,7 @@ export default function Example() {
         ) : (
           <button
             onClick={handleBack}
-            disabled={index === 0 ? true : false}
+            disabled={index === 0 || isLoading ? true : false}
             className="rounded-md bg-gray-600 py-2 text-lg font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 sm:px-20 px-2"
           >
             BACK
@@ -611,9 +614,34 @@ export default function Example() {
 
         <button
           type="submit"
+          disabled={isLoading}
           className="rounded-md bg-indigo-600 py-2 text-lg font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 sm:px-20 px-2"
         >
-          {index === 0 ? "NEXT PAGE" : "SUBMIT"}
+          {isLoading ? (
+            <div className="mx-10">
+              <svg
+                aria-hidden="true"
+                className="w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+                viewBox="0 0 100 101"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                  fill="currentColor"
+                />
+                <path
+                  d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                  fill="currentFill"
+                />
+              </svg>
+              <span className="sr-only">Loading...</span>
+            </div>
+          ) : index === 0 ? (
+            "NEXT PAGE"
+          ) : (
+            "SUBMIT"
+          )}
         </button>
       </div>
     </form>
